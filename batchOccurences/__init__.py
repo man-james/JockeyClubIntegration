6657377ec49ed2ff47a3ccac2e731a54adda70f1
James <james.man@gmail.com>
2022-11-30 23:44:23 +0800

    Break infinite loop in upsert

--- batchOccurences/__init__.py
+++ batchOccurences/__init__.py
@@ -111,6 +111,7 @@ def upsertVOs(accessToken, list):
         r = requests.post(f"http://{jc_api_url}/{jc_api_upsert_path}", json=list, headers=head)
         if r.status_code == 200:
             logging.info(r.json())
+            return
         else:
             wait = retries * 3 
             time.sleep(wait)

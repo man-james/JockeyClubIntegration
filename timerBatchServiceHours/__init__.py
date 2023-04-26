d114a541593bdd54f353d56a6775a153f127d8e9
James <james.man@gmail.com>
2023-04-26 19:56:51 +0800

    create TimeTrigger batchServiceHours

---
+++ timerBatchServiceHours/__init__.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import os
+import requests
+import azure.functions as func
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    cache_occurrences_url = os.environ['THIS_API_URL'] + '/batchservicehours?code=' + os.environ['BATCH_SERVICE_HOURS_FUNCTION_CODE']
+
+    r = requests.get(cache_occurrences_url)
+
+    if r.status_code == 200:
+        logging.info(r.text)
+    else:
+        logging.error(f"Received status code {r.status_code}")
+

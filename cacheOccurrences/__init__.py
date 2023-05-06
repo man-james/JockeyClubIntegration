4ebd4e9de97650b8a02db447da80b601afd98fe6
James <james.man@gmail.com>
2023-05-06 08:33:38 +0800

    fix update bug in query

--- cacheOccurrences/__init__.py
+++ cacheOccurrences/__init__.py
@@ -10,17 +10,29 @@ import json
 
 import azure.functions as func
 
-db_url = os.environ['DB_URL']
-db = os.environ['DB']
-db_username = os.environ['DB_USERNAME']
-db_password = os.environ['DB_PASSWORD']
-db_driver = os.environ['DB_DRIVER']
+db_url = os.environ["DB_URL"]
+db = os.environ["DB"]
+db_username = os.environ["DB_USERNAME"]
+db_password = os.environ["DB_PASSWORD"]
+db_driver = os.environ["DB_DRIVER"]
 
-#serverless DB retry
+# serverless DB retry
 for i in range(0, 4):
     while True:
         try:
-            cnxn = pyodbc.connect('DRIVER='+db_driver+';SERVER='+db_url+';PORT=1433;DATABASE='+db+';UID='+db_username+';PWD='+db_password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+            cnxn = pyodbc.connect(
+                "DRIVER="
+                + db_driver
+                + ";SERVER="
+                + db_url
+                + ";PORT=1433;DATABASE="
+                + db
+                + ";UID="
+                + db_username
+                + ";PWD="
+                + db_password
+                + ";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+            )
         except pyodbc.Error as ex:
             time.sleep(2.0)
             continue
@@ -28,35 +40,47 @@ for i in range(0, 4):
 
 cursor = cnxn.cursor()
 
-hohk_api_url = os.environ['HOHK_API_URL']
-hohk_api_username = os.environ['HOHK_API_USERNAME']
-hohk_api_password = os.environ['HOHK_API_PASSWORD']
+hohk_api_url = os.environ["HOHK_API_URL"]
+hohk_api_username = os.environ["HOHK_API_USERNAME"]
+hohk_api_password = os.environ["HOHK_API_PASSWORD"]
 
-occurrence_url_prefix = os.environ['THIS_API_URL'] + '/occurrence?code=' + os.environ['OCCURRENCE_FUNCTION_CODE'] + '&occurrenceId='
-jobmap_url = os.environ['THIS_API_URL'] + '/jobmap?code=' + os.environ['JOBMAP_FUNCTION_CODE']
+occurrence_url_prefix = (
+    os.environ["THIS_API_URL"]
+    + "/occurrence?code="
+    + os.environ["OCCURRENCE_FUNCTION_CODE"]
+    + "&occurrenceId="
+)
+jobmap_url = (
+    os.environ["THIS_API_URL"] + "/jobmap?code=" + os.environ["JOBMAP_FUNCTION_CODE"]
+)
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Call cacheOccurences function.')
+    logging.info("Call cacheOccurences function.")
     start_time = time.time()
     r = requests.get(jobmap_url)
     json_response = r.json()
     batch_size = 100
 
-    #sending it to SOLR with too many occurrenceIds (100+) seems to cause problems
+    # sending it to SOLR with too many occurrenceIds (100+) seems to cause problems
     l = []
     for occurrence_batch in batched(json_response, batch_size):
-        occurrenceIds = ','.join(occurrence_batch)
+        occurrenceIds = ",".join(occurrence_batch)
         r2 = requests.get(occurrence_url_prefix + occurrenceIds)
 
         if r2.status_code == 200:
             l.extend(r2.json())
         else:
-            logging.error(f"Received status code {r2.status_code} for {occurrence_url_prefix + occurrenceIds}")
-    
-    #here, l contains everything that is a valid occurrence in SOLR
-    #search DB for json_response where occurrenceId in jobmap results
-    ids = (',').join(f"'{w}'" for w in json_response)
-    rows = cursor.execute(f"SELECT occurrenceId, json FROM occurrences WHERE occurrenceId IN ({ids})").fetchall()
+            logging.error(
+                f"Received status code {r2.status_code} for {occurrence_url_prefix + occurrenceIds}"
+            )
+
+    # here, l contains everything that is a valid occurrence in SOLR
+    # search DB for json_response where occurrenceId in jobmap results
+    ids = (",").join(f"'{w}'" for w in json_response)
+    rows = cursor.execute(
+        f"SELECT occurrenceId, json FROM occurrences WHERE occurrenceId IN ({ids})"
+    ).fetchall()
     in_db_ids = [row.occurrenceId for row in rows]
     not_in_db_ids = [id for id in json_response if id not in in_db_ids]
     in_db_json = [row.json for row in rows]
@@ -66,7 +90,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     same_records = 0
 
     for dict in l:
-        occurrenceId = dict.get('vmpJobId')
+        occurrenceId = dict.get("vmpJobId")
         index = -1
         try:
             index = in_db_ids.index(occurrenceId)
@@ -74,35 +98,52 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
 
         if index >= 0:
-            #if in db, is json the same?
+            # if in db, is json the same?
             db_json_dict = json.loads(in_db_json[index])
             if db_json_dict == dict:
-                #logging.info("Same JSON, do nothing")
+                # logging.info("Same JSON, do nothing")
                 same_records += 1
             else:
-                #logging.info("Different JSON, set send=1")
-                cursor.execute(f"UPDATE occurrences SET send=1, error='', json='{json.dumps(dict)}', updatedAt='{time.strftime('%Y-%m-%d %H:%M:%S')}' WHERE occurrenceId='{occurrenceId}'")
+                # logging.info("Different JSON, set send=1")
+                cursor.execute(
+                    f"UPDATE occurrences SET send=?, error=?, json=?, updatedAt=? WHERE occurrenceId=?",
+                    1,
+                    "",
+                    json.dumps(dict),
+                    time.strftime("%Y-%m-%d %H:%M:%S"),
+                    occurrenceId,
+                )
                 cnxn.commit()
                 updated_records += 1
         else:
-            #if not in db, insert directly to db and set send bit to 1
-            cursor.execute(f"INSERT INTO occurrences(occurrenceId, status, json, send, createdAt) VALUES (?, ?, ?, ?, ?)",
-                occurrenceId, "NOT SENT", json.dumps(dict), 1, time.strftime('%Y-%m-%d %H:%M:%S'))
+            # if not in db, insert directly to db and set send bit to 1
+            cursor.execute(
+                f"INSERT INTO occurrences(occurrenceId, status, json, send, createdAt) VALUES (?, ?, ?, ?, ?)",
+                occurrenceId,
+                "NOT SENT",
+                json.dumps(dict),
+                1,
+                time.strftime("%Y-%m-%d %H:%M:%S"),
+            )
             cnxn.commit()
             new_records += 1
 
     end_time = time.time()
 
     return func.HttpResponse(
-        f"Total in SOLR: {len(json_response)}, Not in DB: {len(not_in_db_ids)}, In DB: {len(in_db_ids)}, Inserted: {new_records}, Unchanged: {same_records}, Updated: {updated_records}" + " in " + str(end_time-start_time) + " seconds",
-        status_code=200
+        f"Total in SOLR: {len(json_response)}, Not in DB: {len(not_in_db_ids)}, In DB: {len(in_db_ids)}, Inserted: {new_records}, Unchanged: {same_records}, Updated: {updated_records}"
+        + " in "
+        + str(end_time - start_time)
+        + " seconds",
+        status_code=200,
     )
+
 
 def batched(iterable, n):
     "Batch data into lists of length n. The last batch may be shorter."
     # batched('ABCDEFG', 3) --> ABC DEF G
     if n < 1:
-        raise ValueError('n must be at least one')
+        raise ValueError("n must be at least one")
     it = iter(iterable)
-    while (batch := list(islice(it, n))):
+    while batch := list(islice(it, n)):
         yield batch
